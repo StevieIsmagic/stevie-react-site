40a02942862c5080fdd87c6170b98ef8ca97bfc2
Colombianito66 <colombianito66@gmail.com>
2018-06-21 18:36:13 -0400

    add social cons/hrefs

--- src/components/SocialIcons/SocialIcons.js
+++ src/components/SocialIcons/SocialIcons.js
@@ -6,12 +6,12 @@ const SocialIcons = (props) => {
   return (
     <div className="social-icons animate-icons">
       <a target="_blank" rel="noopener noreferrer" href="https://github.com/stevieismagic"><i className="fab fa-github"></i></a>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/ocamposteven/"><i className="fab fa-linkedin"></i>linkedIn</a>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/stevie.ismagic"><i className="fab fa-github"></i>fb</a>
-      <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/stevieismagic1"><i className="fab fa-twitter"></i>twttr</a>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/stevieismagic/"><i className="fab fa-github"></i>IG</a>
-      <a target="_blank" rel="noopener noreferrer" href="  "><i className="fab fa-github"></i></a>
-      <a target="_blank" rel="noopener noreferrer" href="  "><i className="fab fa-github"></i>RESUME</a>
+      <a target="_blank" rel="noopener noreferrer" href="#"><i className="far fa-file-pdf"></i></a>
+      <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/ocamposteven/"><i className="fab fa-linkedin"></i></a>
+      <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/stevieismagic1"><i className="fab fa-twitter"></i></a>
+      <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/stevie.ismagic"><i className="fab fa-facebook"></i></a>
+      <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/stevieismagic/"><i className="fab fa-instagram"></i></a>
+      <a target="_blank" rel="noopener noreferrer" href="https://github.com/StevieIsmagic/stevie-react-site"><i className="far fa-file-code"></i></a>
     </div>
   )
 }
