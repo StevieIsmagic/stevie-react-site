c6bcdb15d3a4c881031ad805db13e1ebcb59a6d0
Colombianito66 <colombianito66@gmail.com>
2018-06-23 02:21:25 -0400

    accessibility: add title attribute

--- src/components/SocialIcons/SocialIcons.js
+++ src/components/SocialIcons/SocialIcons.js
@@ -6,13 +6,13 @@ import Resume from '../../assets/StevenOcampo.pdf';
 const SocialIcons = (props) => {
   return (
     <div className="social-icons animate-icons">
-      <a target="_blank" rel="noopener noreferrer" href="https://github.com/stevieismagic"><i className="fab fa-github"></i></a>
-      <a target="_blank" rel="noopener noreferrer" href={Resume}><i className="far fa-file-pdf"></i></a>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/ocamposteven/"><i className="fab fa-linkedin"></i></a>
-      <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/stevieismagic1"><i className="fab fa-twitter"></i></a>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/stevie.ismagic"><i className="fab fa-facebook"></i></a>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/stevieismagic/"><i className="fab fa-instagram"></i></a>
-      <a rel="noopener noreferrer" href="mailto:stevenocampo@mail.usf.edu"><i className="far fa-envelope"></i></a>
+      <a target="_blank" rel="noopener noreferrer" title="stevieismagic github profile" href="https://github.com/stevieismagic"><i className="fab fa-github"></i></a>
+      <a target="_blank" rel="noopener noreferrer" title="steven ocampo pdf resume" href={Resume}><i className="far fa-file-pdf"></i></a>
+      <a target="_blank" rel="noopener noreferrer" title="steven ocampo linkedIn profile" href="https://www.linkedin.com/in/ocamposteven/"><i className="fab fa-linkedin"></i></a>
+      <a target="_blank" rel="noopener noreferrer" title="steivieismagic twitter page" href="https://twitter.com/stevieismagic1"><i className="fab fa-twitter"></i></a>
+      <a target="_blank" rel="noopener noreferrer" title="stevieismagic facebook profile" href="https://www.facebook.com/stevie.ismagic"><i className="fab fa-facebook"></i></a>
+      <a target="_blank" rel="noopener noreferrer" title="stevieismagic instagram" href="https://www.instagram.com/stevieismagic/"><i className="fab fa-instagram"></i></a>
+      <a rel="noopener noreferrer" title="send an email to stevenocampo@mail.usf.edu email address" href="mailto:stevenocampo@mail.usf.edu"><i className="far fa-envelope"></i></a>
     </div>
   )
 }
