0b1ed1ba58aade06a85428606a208ed888941385
stevieismagic <stevieismagic@gmail.com>
2020-07-23 22:00:10 -0400

    use native fetch

--- src/components/Portfolio/fetchInstagramFeed.js
+++ src/components/Portfolio/fetchInstagramFeed.js
@@ -1,14 +1,17 @@
-import ky from 'ky';
-import proxy from '../../config';
+import proxyUrl from '../../config';
 
-function fetchInstagram() {
+async function fetchInstagram() {
   // GET IG Media Objects from Proxy
   // Add No Cross Origin Resource Sharing
   // How to protct this endpoint ?
   try {
-    const mediaObjects = ky.get(`${proxy}`).json()
-    console.log('MEDIA OBJECTS', mediaObjects)
-    return mediaObjects
+    const response = await fetch(`${proxyUrl}`)
+       .then(res => console.log(JSON.stringify(res)))
+       .then(data => data)
+       .catch(e => console.log(`ERRR - ${e}`))
+    // const mediaObjects = r
+    // console.log('MEDIA OBJECTS', mediaObjects)
+    // return mediaObjects
   } catch(err) {
     console.log(`ERR FETCHING IG MEDIA OBJECTS`, err)
   }
