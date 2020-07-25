58aae6688851a4cf7e7fe86375009c7601e11e45
stevieismagic <stevieismagic@gmail.com>
2020-07-25 19:23:38 -0400

    To-Do notes

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -3,6 +3,7 @@ import PortfolioItem from '../PortfolioItem/PortfolioItem';
 import ScrollToPrevious from '../ScrollToPrevious/ScrollToPrevious';
 import portfolioItems from './portfolio-items';
 import fetchInstagramFeed from './fetchInstagramFeed';
+import { mediaObjects } from './ig-media-objects';
 
 import './Portfolio.css';
 
@@ -12,10 +13,19 @@ const Portfolio = (props) => {
       <div className="content-grid">
         <h1>i like words =) </h1>
         <div className="portfolio-wrapper">
-          {/* {portfolioItems.map((item, i) => (
+
+        
+          {/* TO-DO
+          
+          for each media object, request media_url .
+          make one async bluebird call for all urls ?
+          OR 20 individual requests ? 
+          
+          
+          {mediaObjects.map((item, i) => (
             <PortfolioItem render={item.render} key={i} />
           ))} */}
-  {fetchInstagramFeed() ? [] : []}
+
 
         </div>
       </div>
---
+++ src/components/Portfolio/ig-media-objects.js
@@ -0,0 +1,200 @@
+export const mediaObjects = [
+  {
+    "id": "17877199528646391",
+    "caption": "Would you write me a sonnet ?",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/97337722_1159057267774231_7689689691659853612_n.jpg?_nc_cat=102&_nc_sid=8ae9d6&_nc_ohc=EWPv9gUtF8YAX_gf_Wq&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=ca1281a08e216bd2c99300d3a204069c&oe=5F43EBCD",
+    "username": "stevieismagic",
+    "timestamp": "2020-05-18T21:00:52+0000"
+  },
+  {
+    "id": "17889064570537246",
+    "caption": "And you. For me ?",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/97357558_567820880831407_7342759607723351887_n.jpg?_nc_cat=101&_nc_sid=8ae9d6&_nc_ohc=HObTNPTLqVEAX-lYTTF&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=0e21705947f67bf569a6fa40d06e1f8e&oe=5F428667",
+    "username": "stevieismagic",
+    "timestamp": "2020-05-18T04:08:56+0000"
+  },
+  {
+    "id": "17948744803341597",
+    "caption": "The things I would do for you.✨",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/96753366_646907302830239_5612681722006539856_n.jpg?_nc_cat=111&_nc_sid=8ae9d6&_nc_ohc=me0fxrdJ_nMAX8rkg7t&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=2b65e2d30872bc57d6662f9580d6e2f1&oe=5F43F677",
+    "username": "stevieismagic",
+    "timestamp": "2020-05-14T21:14:49+0000"
+  },
+  {
+    "id": "17895612853501019",
+    "caption": "Y si yo fuera un mago, desaparecería tu dolor ✨",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/97144998_304100480584742_562647122854605447_n.jpg?_nc_cat=105&_nc_sid=8ae9d6&_nc_ohc=lYhyuH1HwjkAX_iZNBW&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=5ed43be9e2bec6f19e27af483f83910c&oe=5F438C68",
+    "username": "stevieismagic",
+    "timestamp": "2020-05-14T00:52:58+0000"
+  },
+  {
+    "id": "17854307264000076",
+    "caption": "Tu eres magia .",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/97963271_136366831349034_4207781318977791608_n.jpg?_nc_cat=108&_nc_sid=8ae9d6&_nc_ohc=PE1c_XgaEycAX-gZwbN&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=09865036eefc5ecc9804bd4a42198aee&oe=5F411644",
+    "username": "stevieismagic",
+    "timestamp": "2020-05-13T00:38:04+0000"
+  },
+  {
+    "id": "17926479199377298",
+    "caption": "Y gracias . Thank you .",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/95263221_189144648786226_7169369802098201544_n.jpg?_nc_cat=109&_nc_sid=8ae9d6&_nc_ohc=o9rYtGwm4nIAX93SwcZ&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=2d7436dc13cc6ec91ea62ee8e2db2589&oe=5F422B74",
+    "username": "stevieismagic",
+    "timestamp": "2020-05-04T03:29:52+0000"
+  },
+  {
+    "id": "17946975988327677",
+    "caption": "A Dios bb .",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/95056092_1075550026163274_3302290881039773258_n.jpg?_nc_cat=105&_nc_sid=8ae9d6&_nc_ohc=RILHRhQ8yvIAX9zbw-c&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=7328ddeadb107e2de5c69a8400e55cd6&oe=5F425BE4",
+    "username": "stevieismagic",
+    "timestamp": "2020-05-01T03:05:45+0000"
+  },
+  {
+    "id": "17845521188100220",
+    "caption": "Mira al cielo .",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/95012758_225611232206432_6114556093291869403_n.jpg?_nc_cat=108&_nc_sid=8ae9d6&_nc_ohc=Y1e2mx8r5BAAX_Qqr9o&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=cc4f14dcc962e211e15dab735c5f2910&oe=5F44118E",
+    "username": "stevieismagic",
+    "timestamp": "2020-04-30T06:17:40+0000"
+  },
+  {
+    "id": "18139603447049740",
+    "caption": "La vida es un proceso .",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/94897666_4288987907781799_5685839997552253770_n.jpg?_nc_cat=102&_nc_sid=8ae9d6&_nc_ohc=H1r0L1jsuYgAX_OsaaG&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=511d1819bf4aa03eeb1976633391309d&oe=5F40D90C",
+    "username": "stevieismagic",
+    "timestamp": "2020-04-29T04:07:34+0000"
+  },
+  {
+    "id": "17871149572711707",
+    "caption": "Mujer Divina .",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/94674139_553529788635360_1158290084351652698_n.jpg?_nc_cat=103&_nc_sid=8ae9d6&_nc_ohc=qaw13uKssXEAX8yuL6j&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=6cf3cfef2b494cd40a6d69b428f7d533&oe=5F442E2D",
+    "username": "stevieismagic",
+    "timestamp": "2020-04-28T06:40:12+0000"
+  },
+  {
+    "id": "17867136841706880",
+    "caption": "..You can cut all the flowers, but you cannot keep Spring from coming 🖤",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/91094862_2371375329820076_8536411656093272710_n.jpg?_nc_cat=105&_nc_sid=8ae9d6&_nc_ohc=M4y9Ss-w6eoAX-tT_hQ&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=4df2a3d210c517537dda1ef5793c8adf&oe=5F41BFD9",
+    "username": "stevieismagic",
+    "timestamp": "2020-03-30T00:52:40+0000"
+  },
+  {
+    "id": "17856339790485030",
+    "caption": "Simple.",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/67602524_377927723140837_6957158806199384143_n.jpg?_nc_cat=101&_nc_sid=8ae9d6&_nc_ohc=xDTWEf2Sv7sAX9h5N6C&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=74443b5b50cd890f717030951471c643&oe=5F43BE9E",
+    "username": "stevieismagic",
+    "timestamp": "2019-08-10T00:08:45+0000"
+  },
+  {
+    "id": "17907693982281670",
+    "caption": "The greatest deception of humankind is to have ever believed that we are separate, One from the Other.",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/49756236_312476726068549_2359897889407858015_n.jpg?_nc_cat=103&_nc_sid=8ae9d6&_nc_ohc=ZByIWro1iNYAX8EyTJe&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=5c00548a204407fda086d905745366ac&oe=5F425631",
+    "username": "stevieismagic",
+    "timestamp": "2019-01-23T06:24:25+0000"
+  },
+  {
+    "id": "17844902368345257",
+    "caption": "Today I sat under the Bodhi Tree and thought about life.",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/49686698_416428079094168_3474006458577876144_n.jpg?_nc_cat=106&_nc_sid=8ae9d6&_nc_ohc=ZwFNpddlqmEAX8woBKp&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=cf43b11646fc0e63320e8b141bb7a598&oe=5F42FC49",
+    "username": "stevieismagic",
+    "timestamp": "2019-01-19T01:44:32+0000"
+  },
+  {
+    "id": "17921599807086519",
+    "caption": "Asking for a friend..?",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/30593674_1404936522943595_3555792420652187648_n.jpg?_nc_cat=111&_nc_sid=8ae9d6&_nc_ohc=5Qj6X2d3UR4AX9UwTr0&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=f5c82b4a7b2296fa0057086207a77af8&oe=5F42B644",
+    "username": "stevieismagic",
+    "timestamp": "2018-04-23T06:05:27+0000"
+  },
+  {
+    "id": "17908269781141958",
+    "caption": "A real quiet night.",
+    "media_type": "VIDEO",
+    "media_url": "https://video-iad3-1.cdninstagram.com/v/t50.2886-16/28998904_203040050459441_3960123220051572774_n.mp4?_nc_cat=104&vs=17858098975241748_2374057124&_nc_vs=HBkcFQAYJEdQaDh1Z0V4TXhYeXFiZ0FBQ1k0ODBVV0wtVTJia1lMQUFBRhUAAsgBACgAGAAbAYgHdXNlX29pbAExFQAAGAAWyO2U9fTg8AEVAigCQzMsF0AWzMzMzMzNGBRkYXNoX2Jhc2VsaW5lXzY0MF92MREAdeoHAA%3D%3D&_nc_sid=59939d&efg=eyJ2ZW5jb2RlX3RhZyI6InZ0c192b2RfdXJsZ2VuLjY0MC5mZWVkIn0%3D&_nc_ohc=bdU1cbHNtVAAX-_ANNw&_nc_ht=video-iad3-1.cdninstagram.com&oh=56a9c5deb626f866c42343b22be8bfca&oe=5F421FA3&_nc_rid=dc5f7b3c9f",
+    "username": "stevieismagic",
+    "timestamp": "2018-03-24T06:27:57+0000"
+  },
+  {
+    "id": "17863294810154316",
+    "caption": "Change. Or be changed, lover. 🖤",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/19765260_264111480760253_146150517242855424_n.jpg?_nc_cat=110&_nc_sid=8ae9d6&_nc_ohc=e54uFJH01WsAX_l9Qzz&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=ea0a9c943f4ddc20f31c3297d5fba654&oe=5F42AC43",
+    "username": "stevieismagic",
+    "timestamp": "2017-07-09T08:10:21+0000"
+  },
+  {
+    "id": "17859248026182083",
+    "caption": "🖤",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/19050377_1842954886031433_5401768522659397632_n.jpg?_nc_cat=101&_nc_sid=8ae9d6&_nc_ohc=7VgAabMPO94AX9qjjHk&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=c1613f3b9468191a36f9915260870678&oe=5F440625",
+    "username": "stevieismagic",
+    "timestamp": "2017-06-12T12:34:43+0000"
+  },
+  {
+    "id": "17870979823123052",
+    "caption": "..is it true?",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/18950402_445451969165854_5611045610225401856_n.jpg?_nc_cat=103&_nc_sid=8ae9d6&_nc_ohc=m-9SyZrhxnAAX9XNw16&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=633976ae797af3afbe64c4dcee841f47&oe=5F4081EB",
+    "username": "stevieismagic",
+    "timestamp": "2017-06-12T05:07:13+0000"
+  },
+  {
+    "id": "17854270909190838",
+    "caption": "Life is a long lesson in humility. It is healthy to be reminded that the strongest might weaken and the wisest might err.",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/18095058_280499875730224_2084151941448859648_n.jpg?_nc_cat=102&_nc_sid=8ae9d6&_nc_ohc=ml5MQv4_fmwAX__GX2l&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=73f47bcb48b3cca46a337535b51b985e&oe=5F423C8E",
+    "username": "stevieismagic",
+    "timestamp": "2017-04-26T10:35:01+0000"
+  },
+  {
+    "id": "17878121686053035",
+    "caption": "Question.",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/18095731_245248975941313_2305313323781980160_n.jpg?_nc_cat=110&_nc_sid=8ae9d6&_nc_ohc=ZTmmvXgKouYAX__55nb&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=ae4aaee9047dc74dc2636436a9685118&oe=5F41E286",
+    "username": "stevieismagic",
+    "timestamp": "2017-04-22T07:15:57+0000"
+  },
+  {
+    "id": "17878142704024756",
+    "caption": "👽🖤👽",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/18011417_1884993695115959_6851018139052802048_n.jpg?_nc_cat=107&_nc_sid=8ae9d6&_nc_ohc=mZKI4NDVqd8AX8_Wzzb&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=08b83c5415d41b3ab37ba6c462c33c35&oe=5F436385",
+    "username": "stevieismagic",
+    "timestamp": "2017-04-17T11:45:22+0000"
+  },
+  {
+    "id": "17878645765025013",
+    "caption": "The best revenge is not to be like your enemy \n#sweet #revenge #starcrossed #lovers #time #moves #forward #startup #namaste #ahimsa #stevie #sattva #god #thanks #gratitude #instamood #alien #girl #green #iv",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/17662234_1071122042992328_5538997950446829568_n.jpg?_nc_cat=106&_nc_sid=8ae9d6&_nc_ohc=DgZV-KD7DSAAX8_3zEF&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=f82da54ef1005af2e5f773edf1407f9e&oe=5F441163",
+    "username": "stevieismagic",
+    "timestamp": "2017-04-15T03:58:47+0000"
+  },
+  {
+    "id": "17868494854076907",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/17881878_1365110306898070_266444768723075072_n.jpg?_nc_cat=107&_nc_sid=8ae9d6&_nc_ohc=XALSDupfUB8AX-5N5f9&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=07d21319aa2beb953fa677e2f3ce1724&oe=5F40FEB5",
+    "username": "stevieismagic",
+    "timestamp": "2017-04-11T08:13:18+0000"
+  },
+  {
+    "id": "17855872429136082",
+    "media_type": "IMAGE",
+    "media_url": "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/17818836_155497631644209_9214978188994674688_n.jpg?_nc_cat=101&_nc_sid=8ae9d6&_nc_ohc=NpLWMcx9LjoAX-dZHnl&_nc_ht=scontent-iad3-1.cdninstagram.com&oh=2e844b2c51c504f0fd2c597c86b270cd&oe=5F40A54E",
+    "username": "stevieismagic",
+    "timestamp": "2017-04-10T10:56:00+0000"
+  }
+]
