2614fa2ebe7e36093129a209a83411744392cb26
stevieismagic <stevieismagic@gmail.com>
2020-08-07 19:18:22 -0400

    init IG component

---
+++ src/components/InstagramFeed/Instagram.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Instagram from './Instagram.css';
+
+const Instagram = () => {
+  return (
+    <div className='instagram-page'>
+      <h1>IG FEED HERE</h1>
+    </div>
+  );
+};
+
+export default Instagram;
---
+++ src/components/InstagramFeed/fetchInstagramFeed.js
@@ -0,0 +1,22 @@
+
+
+export default async function fetchInstagram() {
+  // GET IG Media Objects from Proxy
+  // Add No Cross Origin Resource Sharing
+  // How to protct this endpoint ?
+
+  // What format is Proxy returning ? - JSON ?
+  try {
+    const url = `https://morning-temple-26180.herokuapp.com`
+    const response = await fetch(url)
+       .then(res => res.json())
+       .then(data => console.log(`serp`, data))
+       .catch(e => console.log(`ERRR - ${e}`))
+    // const mediaObjects = r
+    // console.log('MEDIA OBJECTS', mediaObjects)
+    // return mediaObjects
+  } catch(err) {
+    console.log(`ERR FETCHING IG MEDIA OBJECTS`, err)
+  }
+
+}
