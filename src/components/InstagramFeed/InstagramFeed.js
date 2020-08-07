af797f80ccedbaa7b80ebecb88f7fda2741d3010
stevieismagic <stevieismagic@gmail.com>
2020-08-07 19:45:29 -0400

    add InstagramFeed component to App

---
+++ src/components/InstagramFeed/InstagramFeed.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './InstagramFeed.css';
+
+const InstagramFeed = () => {
+  return (
+    <div className="instagram-page">
+      <div className="content-grid">
+        <div className="instagram-wrapper">
+          <h1>IG FEED HERE</h1>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default InstagramFeed;
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -11,9 +11,9 @@ const Portfolio = (props) => {
       <div className="content-grid">
         <h1>i like words =) </h1>
         <div className="portfolio-wrapper">
-          {/* {portfolioItems.map((item, i) => (
+          {portfolioItems.map((item, i) => (
             <PortfolioItem render={item.render} key={i} />
-          ))} */}
+          ))}
         </div>
       </div>
       <ScrollToPrevious pageSelector=".about-page" />
