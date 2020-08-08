bffb00203882c9bf7d76738adceb2302ac248448
stevieismagic <stevieismagic@gmail.com>
2020-08-08 18:29:43 -0400

    not perfect

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,10 +6,9 @@ const About = (props) => {
   return (
     <div className="about-page">
       <div className="content-grid">
-        {/* <h1>Click these links to learn more about me ;)</h1> */}
         <div className="about-wrapper"> 
           <div className="about-content">
-            <p> My closest friends call me Stevie . </p>
+            {/* <p> My closest friends call me Stevie . </p>
             <p> I am in love with creating . </p>
             <p> I wrote a book about my life . </p>
             <p> I’m working on a children’s book atm :) </p>
@@ -20,15 +19,36 @@ const About = (props) => {
             <p> I studied Philosophy + Religion @ FIU. </p>
             <p> And Gerontology at USF. </p>
             <p> I love people . </p>
-            <p> And I think a lot of how to help the world . </p>
-            <p> I'm not perfect . </p>
+            <p> And I think a lot of how to help the world . </p> */}
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
+            <p> I'm not perfect. </p>
             <p> Even so, my actions are sincere. </p>
-            
-            
-            <p id="principles"> 
-              <strong> Simple - Minimalist - Flow </strong>
+            <p id="principles">
+              <strong> @stevieismagic </strong>
             </p>
-        
           </div>
         </div>
       </div>
