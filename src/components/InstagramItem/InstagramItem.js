4e1b5776aa98c8f07587e9e2f552f806012603a6
stevieismagic <stevieismagic@gmail.com>
2020-08-08 18:02:12 -0400

    swrap in anchor tag

--- src/components/InstagramItem/InstagramItem.js
+++ src/components/InstagramItem/InstagramItem.js
@@ -3,15 +3,16 @@ import React from 'react';
 import './InstagramItem.css';
 
 const InstagramItem = ({ item }) => {
-  console.log(`OBJECT`, typeof item.caption)
   return (
-    <div className="instagram-item">
-      <div className="instagram-item__media">
-      <img src={item.media_url}/>
+    <a href="https://www.instagram.com/stevieismagic/">
+      <div className="instagram-item">
+          <div className="instagram-item__media">
+            <img src={item.media_url}/>
+          </div>
+        <div className="instagram-item__title">{item.caption.substring(0,20)}..</div>
       </div>
-  <div className="instagram-item__title">{item.caption.substring(0,20)}..</div>
-    </div>
-  )
+    </a>
+  );
 };
 
 export default InstagramItem;
